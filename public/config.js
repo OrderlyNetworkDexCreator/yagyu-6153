c5eb9816111d92071466a5641d0ab3c92f4033fc
Mario Reder <mario.reder@pm.me>
2026-04-21 15:12:51 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://yagyu.gay",
+  "VITE_SEO_SITE_URL": "https://dex.orderly.network/yagyu-6153/",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
